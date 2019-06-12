55e33ddbb6a8e10aeafdb8b4ad6b8aa16be034c5
CastilloLuis <luisedcastillog@gmail.com>
2019-06-12 13:33:18 -0400

    final push

---
+++ README.md
@@ -0,0 +1,13 @@
+### Simple "Calories Calculator"
+____
+ABOUT:
+> This project was built to play in just 8 hours.
+> Using functional programming skills (closures, currying, functions composition
+
+### Programming languages
+____
+##### FRONTEND: JavaScript, HTML5, BOOTSTRAP.
+
+### Developers
+____
+#### FRONT-END: [Luis E. Castillo G.](http://github.com/CastilloLuis)
--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
 
-const $ = id => document.getElementById(id);
-
 let formInputs = {
   description: $('description'),
   calories: $('calories'),
@@ -14,23 +12,51 @@ let list = [];
 
 const inputEntries = Object.entries(formInputs);
 
+/**
+ * Find element in DOM by id and returns it
+ * @param {*} id 
+ */
+const $ = id => document.getElementById(id);
+
+/**
+ * Declarative function that generate props for html tags
+ * @param {*} obj 
+ */
 const attrsToString = (obj = {}) => {
   const keys = Object.keys(obj);
   const attrs = (keys.map(key => `${key}="${obj[key]}"`)).join(' ');
   return attrs;
 }
 
-const tagAttrs = obj => (content = '') => 
-`<${obj.tag} ${obj.attrs ? ' ' : ''}${attrsToString(obj.attrs)}>${content}</${obj.tag}>`
+/**
+ *  Function that generate html tags using function composition 
+ * @param {*} obj 
+*/
+const tagAttrs = obj => (content = '') => `<${obj.tag} ${obj.attrs ? ' ' : ''}${attrsToString(obj.attrs)}>${content}</${obj.tag}>`
 
+/**
+ * Function to generate tags with attrs or not.
+ * @param {*} t 
+*/
 const tag = t => {
   if(typeof t === 'string') {
-    tagAttrs({tag: t})
+    return tagAttrs({tag: t})
   } else {
-    tagAttrs(t);
+    return tagAttrs(t);
   }
 }
 
+const tableRowTag = tag('tr');
+const tableRow = items => compose(tableRowTag, tableCells)(items);
+
+const tableCell = tag('td'); // generate one cell
+const tableCells = items => items.map(tableCell).join(''); // iterating over items and generate item.length cells
+
+const trashIcon = tag({tag: 'i', attrs: {class: 'fas fa-trash-alt'}})('')
+
+/**
+ * Validates inputs
+*/
 const validateInputs = () => {
   inputEntries.forEach(prop => {
     if (!prop[1].value) {
@@ -43,8 +69,14 @@ const validateInputs = () => {
   if(Object.values(formInputs).every(({ value }) => value)) {
     addToList();
     clearForm();
+    updateTotals();
+    renderItems()
   }
 }
+
+/**
+ * Remove invalid types in inputs that are keypressed
+*/
 
 const removeInvalidType = () => {
   inputEntries.forEach(prop => {
@@ -56,6 +88,9 @@ const removeInvalidType = () => {
   })
 }
 
+/**
+ * Add new item to list.
+*/
 const addToList = () => {
   const { description, calories, carbs, protein } = formInputs;
   const newItem = {
@@ -66,6 +101,55 @@ const addToList = () => {
   }
   list.push(newItem);
 }
+
+/**
+ * Updates totals of list items
+*/
+const updateTotals = () => {
+  let calories = 0, carbs = 0, protein = 0;
+  list.map(item => {
+    calories += item.calories,
+    carbs += item.carbs,
+    protein += item.protein
+  });
+  $('totalCalories').innerText = calories;
+  $('totalCarbs').innerText = carbs;
+  $('totalProtein').innerText = protein;
+}
+
+/**
+ * Remove items from list by index
+ * @param {*} index 
+ */
+const removeItemFromList = (index) => {
+  list.splice(index, 1);
+  updateTotals();
+  renderItems();
+}
+
+/**
+ * Render rows in tbody
+ */
+const renderItems = () => {
+  const table = $('table-body');
+  table.innerHTML = '';
+  const rows = list.map(({ description, calories, carbs, protein }, index) => {
+    const removeButton = tag({
+      tag: 'button', 
+      attrs: {
+        class: 'btn btn-outline-danger',
+        onclick: `removeItemFromList(${index})`,
+      }
+    })(trashIcon);
+    // console.log(removeButton)
+    return tableRow([description, calories, carbs, protein, removeButton]);
+  });
+  table.innerHTML = rows.join('');
+}
+
+/**
+ * Removes all the tbody's child
+*/
 
 const clearForm = () => Object.values(formInputs).forEach(element => element.value = '');
 
